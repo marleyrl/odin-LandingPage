72567369f6cb608b0b1c09bd04da7a519b412f4d
Marley <marleyrlribeiro@gmail.com>
2023-07-18 23:05:09 -0300

    Landing Page Final Project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'odincolor':'#1f2937',
+      },  
+    },
+  },
+  plugins: [],
+}
+
